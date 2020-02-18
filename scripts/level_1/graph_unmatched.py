a6c86b2fdc1ffdcb1ba936df816bd63fbe1747ee
mwinb <mwinberry0101@gmail.com>
2020-02-18 15:09:27 -0800

    Added graph

---
+++ scripts/level_1/graph_unmatched.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+
+import plotly.graph_objects as go
+import pandas as pd
+import sys
+
+data = pd.read_csv(sys.argv[1])
+dataHeader = data.columns.values
+fig = go.Figure(data=[
+    go.Bar(name="unmatched", x=data[dataHeader[1]], y=data[dataHeader[2]]),
+])
+print(dataHeader[1])
+print(data[dataHeader[1]])
+# Change the bar mode
+fig.update_layout(barmode='relative')
+# fig.update_layout(color=inBoundHeader[0])
+fig.update_layout(xaxis_type='category',
+                  xaxis_title=dataHeader[1], yaxis_title=dataHeader[1])
+fig.show()
