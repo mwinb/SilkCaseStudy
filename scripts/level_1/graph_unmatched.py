d7e6d1850f0181a518dcd68e7c70bde1dbf88c06
mwinb <mwinberry0101@gmail.com>
2020-02-18 15:35:18 -0800

    new graph

--- scripts/level_1/graph_unmatched.py
+++ scripts/level_1/graph_unmatched.py
@@ -6,13 +6,21 @@ import sys
 
 data = pd.read_csv(sys.argv[1])
 dataHeader = data.columns.values
-fig = go.Figure(data=[
-    go.Bar(name="unmatched", x=data[dataHeader[1]], y=data[dataHeader[2]]),
-])
-print(data[dataHeader[0]])
-# Change the bar mode
-fig.update_layout(barmode='group')
-# fig.update_layout(color=inBoundHeader[0])
-fig.update_layout(xaxis_type='category',
-                  xaxis_title=dataHeader[0] + "\\" + dataHeader[1], yaxis_title=dataHeader[1])
+dataRows = data.loc[:, :]
+fig = go.Figure()
+for row in dataRows:
+    fig.add_trace(go.Bar(x=row[1], y=row[2], name=row[0]))
+fig.update_layout(barmode='stack', xaxis={'categoryorder': 'category'})
 fig.show()
+
+
+# fig = go.Figure(data=[
+#     go.Bar(name="unmatched", x=data[dataHeader[0]], y=data[dataHeader[2]]),
+# ])
+# print(data[dataHeader[0]])
+# # Change the bar mode
+# fig.update_layout(barmode='group')
+# # fig.update_layout(color=inBoundHeader[0])
+# fig.update_layout(xaxis_type='category',
+#                   xaxis_title=dataHeader[0], yaxis_title=dataHeader[1])
+# fig.show()
