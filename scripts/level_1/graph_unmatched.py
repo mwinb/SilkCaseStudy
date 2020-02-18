106c36c2e458893e2809ff93b1a90c8ca502e918
NetSA <netsa@Unknown.domain>
2020-02-18 15:44:30 -0800

    finished graph

--- scripts/level_1/graph_unmatched.py
+++ scripts/level_1/graph_unmatched.py
@@ -6,21 +6,10 @@ import sys
 
 data = pd.read_csv(sys.argv[1])
 dataHeader = data.columns.values
-dataRows = data.loc[:, :]
+dataRows = data.loc[:, :][1:]
 fig = go.Figure()
-for row in dataRows:
-    fig.add_trace(go.Bar(x=row[1], y=row[2], name=row[0]))
-fig.update_layout(barmode='stack', xaxis={'categoryorder': 'category'})
+for index, row in data.iterrows():
+    fig.add_trace(go.Bar(x=[row[1]], y=[row[2]], name=row[0]))
+fig.update_layout(xaxis_type='category',
+    xaxis_title=dataHeader[1], yaxis_title=dataHeader[2], xaxis={'categoryorder':'category ascending'})
 fig.show()
-
-
-# fig = go.Figure(data=[
-#     go.Bar(name="unmatched", x=data[dataHeader[0]], y=data[dataHeader[2]]),
-# ])
-# print(data[dataHeader[0]])
-# # Change the bar mode
-# fig.update_layout(barmode='group')
-# # fig.update_layout(color=inBoundHeader[0])
-# fig.update_layout(xaxis_type='category',
-#                   xaxis_title=dataHeader[0], yaxis_title=dataHeader[1])
-# fig.show()
